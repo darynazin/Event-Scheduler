3a16de2dd64ee60f0f38dcad219dcb4f083f2952
darynazin <danayu@ukr.net>
2025-01-09 22:38:27 +0100

    feature/linked the 'add event' button to the create event page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router";
+
 function Header() {
+  let navigate = useNavigate();
   return (
     <header className="flex justify-between items-center px-8 py-4 bg-gray-900 w-full">
       <h1 className="text-5xl font-great-vibes text-white">Event Scheduler</h1>
-      <button className="bg-[#CECECE] text-[#1E1E1E] font-bold py-2 px-6 rounded-full hover:bg-[#cbcbcb] focus:outline-none focus:ring-2 focus:ring-blue-500">
+      <button onClick={() => {
+          navigate("/create-event");
+        }} 
+        className="bg-[#CECECE] text-[#1E1E1E] font-bold py-2 px-6 rounded-full hover:bg-[#cbcbcb] focus:outline-none focus:ring-2 focus:ring-blue-500">
         Add Event
       </button>
     </header>
