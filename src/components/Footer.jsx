54c3e88a4ecd587ce946e17eea90a099137e6053
darynazin <danayu@ukr.net>
2025-01-13 23:23:27 +0100

    resolving

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,45 +4,45 @@ function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center gap-4 p-6 bg-gray-800 text-white text-sm">
       <div className="flex flex-wrap justify-center gap-8">
-        <Link to="#" className="hover:underline hover:text-cyan-400">
+        <a href="#" className="hover:underline hover:text-cyan-400">
           About Us
-        </Link>
-        <Link to="#" className="hover:underline hover:text-cyan-400">
+        </a>
+        <a href="#" className="hover:underline hover:text-cyan-400">
           Contact
-        </Link>
-        <Link to="#" className="hover:underline hover:text-cyan-400">
+        </a>
+        <a href="#" className="hover:underline hover:text-cyan-400">
           Privacy Policy
-        </Link>
-        <Link to="#" className="hover:underline hover:text-cyan-400">
+        </a>
+        <a href="#" className="hover:underline hover:text-cyan-400">
           Terms of Service
-        </Link>
+        </a>
       </div>
 
       <div className="flex gap-6 justify-center mt-4">
-        <Link
-          to="#"
+        <a
+          href="#"
           className="text-white hover:text-cyan-400 transition duration-300"
         >
           <i className="fab fa-facebook fa-2x"></i>
-        </Link>
-        <Link
-          to="#"
+        </a>
+        <a
+          href="#"
           className="text-white hover:text-cyan-400 transition duration-300"
         >
           <i className="fab fa-twitter fa-2x"></i>
-        </Link>
-        <Link
-          to="#"
+        </a>
+        <a
+          href="#"
           className="text-white hover:text-cyan-400 transition duration-300"
         >
           <i className="fab fa-instagram fa-2x"></i>
-        </Link>
-        <Link
-          to="#"
+        </a>
+        <a
+          href="#"
           className="text-white hover:text-cyan-400 transition duration-300"
         >
           <i className="fab fa-linkedin fa-2x"></i>
-        </Link>
+        </a>
       </div>
 
       <div className="text-gray-400 text-xs mt-4">
@@ -50,9 +50,9 @@ function Footer() {
       </div>
 
       <div>
-        <Link to="#top" className="hover:underline hover:text-cyan-400">
+        <a href="#top" className="hover:underline hover:text-cyan-400">
           Back to Top ↑
-        </Link>
+        </a>
       </div>
     </footer>
     // </footer>
